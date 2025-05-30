4de06ee3ae4f9c50ad2c259c5d235f15f6bef021
BoraY08 <boray@nycstudents.net>
2025-05-30 16:27:27 +0000

    4

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ grid = [[" "," "," "],[" "," "," "],[" "," "," "]]
 def printGrid(n):
   for i in range(len(n)):
     print(""+n[i][0]+"  |"+n[i][1]+"  |"+n[i][2])
-    
     if i < 2:
       print("---+---+---")
 printGrid(grid)
-q = random.randint(1,2)
-if q == 1:
+turn = random.randint(1,2)
+if turn == 1:
   print("You start!")
-if q == 2:
+if turn == 2:
   print("Computer starts!")
 
 def forkChecker(n,typ,chtyp,opp):
@@ -67,16 +66,16 @@ while True:
     print("Tie")
     break
 #player turn
-  if q == 1:
+  if turn == 1:
     printGrid(grid)
     j = int(input("Pick a row to play (0,2) "))
     f = int(input("Pick a column to play (0,2) "))
     if   (j < 3 and f < 3) and grid[j][f] == " ":
       grid[j][f] = "x"
-      q = 2
+      turn = 2
     else:
       print("invalid tile")
-      q = 1
+      turn = 1
   
     if wincheck(grid,"x") == True:
       printGrid(grid)
@@ -90,47 +89,47 @@ while True:
     if winmov == False:
       winmov = winningmove(grid,"o","o")
     if winmov == True:
-      q =1
+      turn =1
       
     #check for player winning move
     if winmov == False:
       winmov =winningmove(grid,"x","o")
     if winmov == True:
-      q =1
+      turn =1
       
     #check for fork
     if winmov == False:
       winmov = forkChecker(grid,"o","o","x")
     if winmov == True:
-      q =1
+      turn =1
 
     
     #check for opponent fork
     if winmov == False:
       winmov = forkChecker(grid,"x","o","o")
     if winmov == True:
-      q =1
+      turn =1
     #check for center
     if  winmov == False and grid[1][1] == " " :
       grid[1][1] = "o"
-      q = 1
+      turn = 1
       winmov = True
 
     if winmov == False and grid[0][0] == " ":
       grid[0][0] = "o"
-      q = 1
+      turn = 1
       winmov = True  
     if winmov == False and grid[0][2] == " ":
       grid[0][2] == "o"
-      q=1
+      turn=1
       winmov == True
     if winmov == False and grid[2][0] == " ":
       grid[2][0] == "o"
-      q=1
+      turn=1
       winmov == True
     if winmov == False and grid[2][2] == " ":
       grid[2][2] == "o"
-      q=1
+      turn=1
       winmov == True
 
     #random
@@ -139,10 +138,10 @@ while True:
       y = random.randint(0,2)
       if grid[x][y] == " ":
         grid[x][y] = "o"
-        q = 1
+        turn = 1
       else:
-        q = 2
-
+        turn = 2
+ 
     if wincheck(grid,"o") == True:
       printGrid(grid)
       print("Computer wins")
